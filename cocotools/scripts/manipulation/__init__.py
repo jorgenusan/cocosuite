34e9b7c6de4b8596761b8112334f8e97673b9a70
Jorge Núñez Santiago <jorge.nusan@gmail.com>
2024-06-27 18:18:15 +0200

    remove project cli

--- cocotools/scripts/manipulation/__init__.py
+++
@@ -1,7 +0,0 @@
-from cocotools.scripts.manipulation.property_split import property_split
-from cocotools.scripts.manipulation.random_split import random_split
-
-coco_manipulation = {
-    "random-split": random_split,
-    "property-split": property_split,
-}
